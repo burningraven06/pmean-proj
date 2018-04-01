3eaf3cd4d2879ccb1d0783414224733d912d250b
Toufiq <burningraven06@gmail.com>
2018-04-01 19:15:55 +0600

    create new component and use routes

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 
+import {Hero} from '../modelStyles/hero';
+import {HeroService} from '../hero.service';
+
+
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
@@ -7,9 +11,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DashboardComponent implements OnInit {
 
-  constructor() { }
+  constructor(private heroService: HeroService) { }
+  
+  dashboardHeroes: Hero[]
+
+  retrieveRecentHeroes(): void{
+    this.heroService.getHeroes().subscribe(data => this.dashboardHeroes = data.slice(1,4))
+  }
 
   ngOnInit() {
+    this.retrieveRecentHeroes()
   }
 
 }
