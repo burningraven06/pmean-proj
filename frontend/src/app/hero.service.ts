3672ca1d338e683da1dc9efa973ffdae27a71ade
Toufiq <burningraven06@gmail.com>
2018-04-03 01:28:38 +0600

    update angular service for getAll Request, and getByID request

--- frontend/src/app/hero.service.ts
+++ frontend/src/app/hero.service.ts
@@ -5,6 +5,7 @@ import {HEROESDUMMYLIST} from './data/heroes';
 import {Observable} from 'rxjs/Observable';
 import {of} from 'rxjs/observable/of';
 import {HttpClient, HttpHeaders } from '@angular/common/http';
+import { catchError, map, tap } from 'rxjs/operators';
 
 import {MessageService} from './message.service';
 @Injectable()
@@ -14,10 +15,17 @@ export class HeroService {
 
   private heroesURL = 'http://localhost:3000/api/heroes';
 
+  private handleServerError<T> (operation = 'operation', result?: T) {
+    return (error: any): Observable<T> => {
+      console.error(error);
+      this.logMsg(`${operation} failed: ${error.message}`);
+      return of(result as T);
+    };
+  }
+
   getHeroes(): Observable<Hero[]> {
     this.messageService.addMsg('HeroService: Fetched Heroes');
-    // return of(HEROESDUMMYLIST);
-    return this.http.get<Hero[]>(this.heroesURL);
+    return this.http.get<Hero[]>(this.heroesURL).pipe(tap( heroes => this.logMsg(`Fetched Heroes`)), catchError(this.handleServerError('getHeroes', [])));
   }
 
   private logMsg(msg: string) {
@@ -26,7 +34,8 @@ export class HeroService {
 
   getHeroByID(id: number): Observable<Hero> {
     this.messageService.addMsg(`HeroService: fetched hero with id=${id}`);
-    return of(HEROESDUMMYLIST.find(hero => hero.id === id));
+    const getHeroByIDURL = `${this.heroesURL}/${id}`;
+    return this.http.get<Hero>(getHeroByIDURL).pipe( tap(_ => this.logMsg(`Fetched Hero with ID: ${id}`)), catchError(this.handleServerError<Hero>(`getHeroByID: ${id}`)) );
   }
 
 }
